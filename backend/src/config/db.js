9e83499dbb9ff31eb49b8d4267a47cda1441f4d6
Vipin Dagar <vipindagar@Vipins-Air.lan>
2026-08-29 15:39:37 +0530

    change ui of scanner

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+require("dotenv").config({ path: path.join(__dirname, "../../.env") });
 
 async function connectDb() {
   const uri = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/event_platform";
